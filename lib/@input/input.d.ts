b462eaf4999a6d7fbb092819958a3f5c1aead9a1
jayce.thai <phat.tnh@carehealth.io>
2025-04-30 13:10:43 +0700

    chore: release build-only tag v1.0.0

---
+++ lib/@input/input.d.ts
@@ -0,0 +1,6 @@
+import "./input.less";
+export type IInput = {
+    placeholder?: string;
+};
+declare const Input: ({ placeholder }: IInput) => import("react/jsx-runtime").JSX.Element;
+export default Input;
