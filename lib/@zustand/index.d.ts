a3ae02af18162798a9b7333d42a62e0bf5c55f71
jayce.thai <phat.tnh@carehealth.io>
2025-05-14 10:04:43 +0700

    chore: release build-only tag v1.0.6

---
+++ lib/@zustand/index.d.ts
@@ -0,0 +1,4 @@
+export * from "./store";
+export { useShallow } from "zustand/react/shallow";
+export type { DevtoolsOptions, PersistOptions } from "zustand/middleware";
+export type { StateCreator, StoreMutatorIdentifier, StoreApi, UseBoundStore, Mutate, } from "zustand";
---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+export * from "./@input";
+export * from "./@icon";
+export * from "./@label";
+export * from "./@hook";
+export * from "./@combobox";
+export * from "./@button";
