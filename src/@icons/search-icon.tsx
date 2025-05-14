1bd71a1b643816ed412fef041e2087b00d310394
jayce.thai <phat.tnh@carehealth.io>
2025-05-14 21:02:10 +0700

    Pumb version

--- src/@icons/search-icon.tsx
+++ src/@icons/search-icon.tsx
@@ -8,9 +8,9 @@ export const SearchIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     {...props}
   >
     <path d="m21 21-4.34-4.34" />
